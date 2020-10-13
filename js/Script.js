f6d29c229ebe45b86a9211003a9d2d73c655d26d
louisajanssen <67242249+louisajanssen@users.noreply.github.com>
2020-10-12 20:43:30 -0700

    referncing inputs

--- js/Script.js
+++ js/Script.js
@@ -1,14 +1,18 @@
 function CheckIfCanParty() {
     console.log("Check If Can Party")
+    const gradeInput = document.getElementById("grade")
+    const homeworkInput = document.getElementById("homework")
+    console.log(gradeInput.value)
+    console.log(homworkInput.value)
     var grade = 98
     var didHomework = true
-    const htmlElement = document.getElementById("PartyResultBox")
+    const partyResult = document.getElementById("PartyResultBox")
 
     if(grade > 89 && didHomework == true) {
         console.log("Go Party")
-        htmlElement.value = "Go Party"
+        partyResult.value = "Go Party"
     } else {
         console.log("Stay Home")
-        htmlElement.value = "Stay Home"
+        partyResult.value = "Stay Home"
     }
 }
