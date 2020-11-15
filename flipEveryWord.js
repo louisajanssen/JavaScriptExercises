3c205557c1f3cb988ad3f6c70b6978ef8ab9b15f
louisajanssen <janssenlouisa1@gmail.com>
2020-11-15 14:09:24 -0800

    exercises, assertEqual

--- flipEveryWord.js
+++ flipEveryWord.js
@@ -1,16 +1,43 @@
 function flipEveryWord(string) {
-    flipped = '';
+    if (typeof string !== 'string') {
+        return '';
+    }
+    var flipped = '';
     var words = string.split(' ');
 
     for (var i = 0; i < words.length; i++) {
         var currentWord = words[i];
-        var reversed = reverse(currentWord);
+        var reversed = reverseString(currentWord);
         flipped += reversed;
-    }
-    return reversed;
+
+        if (i !== words.length -1) {
+            flipped += ' ';
+        }
+    } 
+    return flipped;
+  }
+
+  function reverseString(string) {
+      var newString= '';
+
+      for (var i = string.length - 1; i > -1; i--) {
+          newString += string[i];
+      }
+      return newString;
   }
   
-  //Example:
-  var input = 'Miko is a good boy';
-  var output = flipEveryWord(input);
-  console.log(output); 
+  function assertEqual(actual, expected, testName) {
+      if (actual === expected) {
+          console.log('passed ' + testName)
+      } else {
+          console.log('FAILED [' + testName + '] expected ' + expected + ' but got ' + actual)
+      }
+  }
+
+  assertEqual(flipEveryWord('Miko is a good boy'), 'okiM si a doog yob', 'flips every word in string') 
+  assertEqual(flipEveryWord('Miko'), 'okiM', 'single word') 
+  assertEqual(flipEveryWord(''), '', 'empty string')
+  assertEqual(flipEveryWord('M'), 'M', 'single letter')
+  assertEqual(flipEveryWord('Waschmaschine'), 'enihcsamhcsaW', 'long word') 
+  assertEqual(flipEveryWord([0, 1]), '', 'array with numbers') 
+  assertEqual(flipEveryWord(0), '', 'number') 
--- removeLargestNum.js
+++ removeLargestNum.js
@@ -1,18 +1,72 @@
 function removeLargestNum(array){
+
+    if (Array.isArray(array) === false) {
+        return [-1];
+    }
+
+    for (var i = 0; i < array.length; i++) {
+        if (typeof array[i] !== 'number') {
+            return [-1];
+        }
+    }
+
     var largest = array[0];
     for (var i = 0; i < array.length; i++) {
         if (array[i] > largest) {
             largest = array[i]
         }
     }
-    console.log('Largest number is', largest)
-    var indexOfLargest = arr.indexOf(largest)
-    console.log('Index of largest:', indexOfLargest)
-    array.splice(indexOfLargest, 1);
-    return array;
+    
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] !== largest) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 }
 
 
-var input = [1, 2, 100, 4, 5]
-var output = removeLargestNum(input);
-console.log(output);
+function areArraysEqual(actual, expected) {
+    if (actual.length !== expected.length) {
+        return false;
+    } else {
+        for (var i = 0; i < actual.length; i++) {
+            if (actual[i] !== expected[i]) {
+                return false;
+            }
+    }
+}
+return true;
+}
+
+function assertEqual (actual, expected, testName) {
+    if (areArraysEqual(actual, expected)) {
+        console.log('passed ' + testName)
+    } else {
+        console.log('FAILED ' + testName + ' expected [' + expected + '] but got ' + actual)
+    }
+}
+
+assertEqual(removeLargestNum([1, 2, 3, 4, 5]), [1, 2, 3, 4], 'removes largest number')
+
+assertEqual(removeLargestNum([]), [], 'returns empty array')
+
+assertEqual(removeLargestNum([1, 2, 3, 4, 5, 5]), [1, 2, 3, 4], 'removes both largest number')
+
+assertEqual(removeLargestNum([1, 2, true, 4, 5]), [-1], 'returns [-1]')
+
+assertEqual(removeLargestNum([-1, 2, 3, 4, 5]), [-1, 2, 3, 4], 'removes largest number, ignores minus')
+
+assertEqual(removeLargestNum([0]), [], 'removes largest number 0')
+
+assertEqual(removeLargestNum([1, 2, 'Miko', 4, 5]), [-1], 'returns [-1],')
+
+assertEqual(removeLargestNum(['Miko is a good boy']), [-1], 'returns [-1]')
+
+assertEqual(removeLargestNum(5), [-1], 'returns [-1]')
+
+assertEqual(removeLargestNum([5, 1, 2, 3, 4, 5]), [1, 2, 3, 4], 'removes both largest number')
+
+assertEqual(removeLargestNum([5, 5, 5, 5, 5]), [], 'removes all largest number')
