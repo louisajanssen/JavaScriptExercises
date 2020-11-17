22a9d0c5cbd47c14c4fa5e300d175dfac3a90405
louisajanssen <janssenlouisa1@gmail.com>
2020-11-16 22:11:47 -0800

    exercises

---
+++ Transpose.js
@@ -0,0 +1,22 @@
+// You will be given an array that contains two strings. Your job is to create a function that will take those two strings and transpose them, so that the strings go from top to bottom instead of left to right.
+function transposeTwoStrings(array) {
+    var string1 = array[0];
+    var string2 = array[1];
+    var result = '';
+
+    for (var i = 0; i < string1.length; i++) {
+        result += string1[i] + ' ' + string2[i] + '\n'
+    }
+
+    return result;
+}
+
+var output = transposeTwoStrings(['Hello','World']);
+console.log(output);
+
+// should return:
+// H W  
+// e o  
+// l r  
+// l l  
+// o d
